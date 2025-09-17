455f4c91227131d6555061db4cef36649d6adf20
kietsocola <vankiet27012004@gmail.com>
2025-09-17 15:00:41 +0700

    updae CV

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <!-- Google tag (gtag.js) -->
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-HSTCSTWF9J"></script>
         <script>
           window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+          function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
         
           gtag('config', 'G-HSTCSTWF9J');
