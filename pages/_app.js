c97906fa9bf3fa3c76db46570bff093fa719d525
kietsocola <vankiet27012004@gmail.com>
2025-09-17 15:04:07 +0700

    update Id gg analyst

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,27 @@ import "../styles/global.css"
 export default function App({ Component, pageProps }) {
 
     return (
-        <Component {...pageProps} />
+        <>
+            {/* Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=G-HSTCSTWF9J`}
+            />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HSTCSTWF9J');
+          `,
+                }}
+            />
+
+            <Component {...pageProps} />
+        </>
     )
 }
 
